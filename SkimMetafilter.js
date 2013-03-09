aed99e26e9bafa98944912652b8115b21e2a5346
Vikas Gorur <vikas@idlecuriosity.info>
2013-03-08 23:27:59 -0800

    Prefer newer comments for deletion when favorite counts are equal.

--- SkimMetafilter.js
+++ SkimMetafilter.js
@@ -1,6 +1,20 @@
 (function () {
   var debug = true;
 
+  // Given the div element of a comment, return its timestamp
+  var timestampForComment = function(commentDiv) {
+    var regex  = /(\d+):(\d+).*(January|February|March|April|May|June|July|August|September|October|November|December) (\d+)/;
+    var text = $($(commentDiv).find('.smallcopy')).text();
+    var pieces = text.match(regex);
+
+    if (pieces === null) {
+      // This is the div for the comment box at the end of the page.
+      return 0;
+    } else {
+      return (Date.parse(pieces[0].replace(/on/, '')));
+    }
+  }
+
   // Given the div element of a comment, return its favorite count
   var favoriteForComment = function(commentDiv) {
     var favSpan = $(commentDiv).find('span').filter(function () {
@@ -17,30 +31,42 @@
 
   var filterComments = function () {
     var totalFavorites = 0;
-    var favorites = [];
-    var comments = [];
+    var favorites      = [];
+    var comments       = [];
 
     $('.comments').each(function (index, elem) {
-      var fav = favoriteForComment(elem);
+      var fav       = favoriteForComment(elem);
+      var timestamp = timestampForComment(elem);
 
-      comments.push({'div': elem, 'favorite': fav});
+      comments.push({'div': elem, 'favorite': fav, 'timestamp': timestamp});
       favorites.push(fav);
       totalFavorites += fav;
     });
 
     var filterThreshold = totalFavorites * 0.9;
 
-    comments.sort(function (a, b) {
-      if (a.favorite > b.favorite) {
+    // Sorts a, b in decreasing order
+    var intCompare = function(a, b) {
+      if (a > b) {
         return -1;
-      } else if (a.favorite < b.favorite) {
+      } else if (a < b) {
         return 1;
       } else {
         return 0;
       }
+    };
+
+    comments.sort(function (a, b) {
+      var favResult = intCompare(a.favorite, b.favorite);
+      if (favResult === 0) {
+        // Prefer older comments to newer ones
+        return (intCompare(b.timestamp, a.timestamp));
+      } else {
+        return favResult;
+      }
     });
 
-    var favoriteSum = 0;
+    var favoriteSum  = 0;
     var deletedCount = 0;
 
     // Step through the comments until we accumulate enough to satisfy
