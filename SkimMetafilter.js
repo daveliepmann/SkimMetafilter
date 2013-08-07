2cb5dceab16770a958051a41a72a63dea3fd2276
Dave Liepmann <dave.liepmann@gmail.com>
2013-08-06 22:02:48 -0400

    Additional styling for favorites count

--- SkimMetafilter.js
+++ SkimMetafilter.js
@@ -143,7 +143,10 @@
         });
 
         // Style the removed comments
-        var style = $("<style>.dimmedForSkimming { color: red; }</style>");
+        var style = $("<style>
+                      .dimmedForSkimming { color: #aaa; }
+                      .dimmedForSkimming a { color: red; }
+                      </style>");
         $('html > head').append(style);
 
 
